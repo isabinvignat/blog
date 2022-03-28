ff1eaaad45918aa250f83cfc615dbe0eb45676be
isabinvignat <ibinvignat@gmail.com>
2022-03-28 19:55:01 -0300

    creando tabla

--- desafioguiado.sql
+++ desafioguiado.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE posts;
 
-CREATE TABLE  post(id VARCHAR(25), nombre usuario VARCHAR(25)
-fecha de creacion DATE, contenido VARCHAR(255), descripcion
-VARCHAR(255), PRIMARY KEY (id));
+CREATE TABLE posts(id VARCHAR(25), nombre usuario
+VARCHAR(25), fecha de creacion DATE, contenido VARCHAR(255), descripcion
+VARCHAR (25), PRIMARY KEY (id) );
